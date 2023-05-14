db7b512dc73e9012523cca9a9364ae3ab98daab3
Aditi <adtsolanki22@gmail.com>
2023-05-14 16:04:02 +0530

    queryId

--- src/components/hooks/useUserData.js
+++ src/components/hooks/useUserData.js
@@ -3,13 +3,14 @@ import axios from "axios";
 
 export const useUserData = (userId) => {
 
-const fetchUserDetail = (userId) => {
+const fetchUserDetail = ({queryKey}) => {
+    const userId = queryKey[1];
     console.log("fetch user detail",userId);
     return axios.get(`http://localhost:3001/users/${userId}`)
 }
     return useQuery(
             ['fetch-user', userId],
-            () => fetchUserDetail(userId)
+            fetchUserDetail
     )   
 }
 
