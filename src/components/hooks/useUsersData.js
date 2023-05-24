fa64f364c305f785770d12cf12acef8112eca852
Aditi <adtsolanki22@gmail.com>
2023-05-24 16:13:54 +0530

    Optimistic updates

--- src/components/hooks/useUsersData.js
+++ src/components/hooks/useUsersData.js
@@ -32,14 +32,40 @@ const addUser = (user) => {
 export const useAddUserData = () => {
     const queryClient = useQueryClient();
     return useMutation(addUser, {
-        onSuccess: (data) => {
-            // queryClient.invalidateQueries('fetch-users')
+        // onSuccess: (data) => {
+        //     // queryClient.invalidateQueries('fetch-users')
+        //     queryClient.setQueriesData('fetch-users', (oldQueryData) => {
+        //         return {
+        //             ...oldQueryData,
+        //             data: [...oldQueryData.data, data.data]
+        //         }
+        //     })
+        // },
+        onMutate: async (newUser) => {
+            await queryClient.cancelQueries('fetch-users')
+            const previousUserData = queryClient.getQueryData('fetch-users')
             queryClient.setQueriesData('fetch-users', (oldQueryData) => {
+                    return {
+                        ...oldQueryData,
+                        data: [
+                            ...oldQueryData.data,
+                            {
+                                id: oldQueryData.data.length + 1,
+                                ...newUser 
+                            },
+                        ],
+                    }
+                })
                 return {
-                    ...oldQueryData,
-                    data: [...oldQueryData.data, data.data]
+                    previousUserData,
                 }
-            })
+            },
+        onError: (_error, _username, context ) => {
+            queryClient.setQueryData('fetch-users',context.previousUserData)
+        },
+
+        onSettled: () => {
+            queryClient.invalidateQueries('fetch-users')
         }
     })
 }
